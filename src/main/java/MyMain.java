6e8ed9db037e03deed7113b9924bfe5db664e37c
MetalTurtle <dekolis1218@gmail.com>
2021-02-04 10:44:03 -0800

    Create three recursive methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,30 +4,26 @@ public class MyMain {
 
     // Wrapper method
     public static boolean isSorted(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return false;
+        return isSortedRec(list, list.size() - 1);
     }
 
     // This recursive method checks if the array is sorted in 
     // non-decreasing order
     public static boolean isSortedRec(ArrayList<Integer> list, int i) {
-        // YOUR CODE HERE
-        return false;
+        return i == 0 || (list.get(i - 1) <= list.get(i) && isSortedRec(list, --i));
     }
 
 
 
      // Wrapper method
     public static boolean hasCountCopies(int[] arr, int x, int count) {
-        // YOUR CODE HERE
-        return false;
+        return hasCountCopiesRec(arr, x, count, arr.length - 1);
     }
 
     // This recursive method checks if the array contains exactly
     // count copies of the integer x
     public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i) {
-        // YOUR CODE HERE
-        return false;
+        return i == 0 ? ((arr[i] == x ? 1 : 0) == count) : (hasCountCopiesRec(arr, x, count - (arr[i] == x ? 1 : 0), --i));
     }
 
 
@@ -35,14 +31,16 @@ public class MyMain {
 
     // Wrapper method
     public static boolean binarySearch(int[] arr, int num) {
-        // YOUR CODE HERE
-        return false;
+        return binarySearchRec(arr, num, 0, arr.length - 1);
     }
 
     // This recursive method calls binary search on the array
     public static boolean binarySearchRec(int[] arr, int num, int lowerBound, int upperBound) {        
-        // YOUR CODE HERE
-        return false;
+        int middleIndex = lowerBound + (upperBound - lowerBound) / 2;
+        if (lowerBound > upperBound) return false;
+        if (arr[middleIndex] > num) return binarySearchRec(arr, num, lowerBound, middleIndex - 1);
+        if (arr[middleIndex] < num) return binarySearchRec(arr, num, middleIndex + 1, upperBound);
+        return true;
     }
 
     public static void main(String[] args) {
